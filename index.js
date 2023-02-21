8a158eed5008dd8e88ece5268176227ed8971241
Nishimwe Prince <princeelysee@gmail.com>
2023-02-21 10:16:55 +0200

    Completed donation page

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ hamburger_menu[1].addEventListener("click", () => {
     hamburger_menu[1].classList.toggle("is-not-active");
     responsive_nav.classList.toggle("hide-nav");
 });
+
+// DONATE PAGE
+
+// Path: donate.html
+
+let donate_currencies = document.getElementById("donate-currencies");
+let donate_amount = document.getElementById("donate-amount");
+
+donate_currencies.addEventListener("change", (event) => {
+
+    let selectedCurrency = event.target.value;
+
+    console.log(selectedCurrency);
+
+});
